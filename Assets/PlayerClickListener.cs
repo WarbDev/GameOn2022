c4130090a5bd981181e6780196c1ae2468cb7ec9
Daniel <63262459+kuckman@users.noreply.github.com>
2022-11-04 13:56:20 -0700

    Update PlayerClickListener.cs

--- Assets/PlayerClickListener.cs
+++ Assets/PlayerClickListener.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PlayerClickListener : MonoBehaviour
+public class PlayerClickListener : MonoBehaviour, IObservePlayers
 {
     [SerializeField] List<ClickableEntity> players;
+    public event Action<Player> PlayerClick;
 
+    public void OnPlayerAdded(Player player)
+    {
+        player.GetComponent<ClickableEntity>().EntityClicked += InvokeClickEvent;
+    }
+
+    public void OnPlayerRemoved(Player player)
+    {
+        player.GetComponent<ClickableEntity>().EntityClicked -= InvokeClickEvent;
+    }
+
+    void InvokeClickEvent(ClickableEntity playerClickableEntity)
+    {
+        PlayerClick?.Invoke(playerClickableEntity.gameObject.GetComponent<Player>());
+    }
 }
