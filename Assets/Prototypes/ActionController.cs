ba6444668446f439e3ec3201d335a24673ae9233
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-11-04 15:43:06 -0700

    Made Prototype Scene for Action Prototype

---
+++ Assets/Prototypes/ActionController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Shape
+{
+    Square,
+    Otherthings
+}
+
+public class ActionController: MonoBehaviour
+{
+    [SerializeField] ClickListener clicky;
+
+    public Animator animator;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            animator.SetBool("isBall", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.J))
+        {
+            animator.SetBool("isBall", false);
+        }
+    }
+
+    public void onCLick()
+    {
+
+    }
+}
---
+++ Assets/Prototypes/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Prototype", menuName = "ScriptableObjects/Prototypething")]
+/* This is a fireball
+ * that deals damage in an area
+ * That has a chance to deal more damage.
+ * 
+ * It makes the player do an animation
+ * has an animation of moving towards the selected point
+ * and does an explosion animation there
+ * 
+ * and has variable range.
+ */
+
+public class NewBehaviourScript : ScriptableObject
+{
+    public int damage;
+    public int damageSecondary;
+    public float SecondaryChance = .5f;
+
+    public Shape rangeShape = Shape.Square;
+    public int rangeRadius = 5;
+
+    public Shape fireShape = Shape.Square;
+    public int fireRadius = 2;
+}
