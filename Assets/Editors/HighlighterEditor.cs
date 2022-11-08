467c6109bb684df53724409b5a20ffd7bacb1fc6
Daniel <63262459+kuckman@users.noreply.github.com>
2022-11-08 12:46:11 -0800

    Updated highlighter logic to allow for greater modularity

---
+++ Assets/Editors/HighlighterEditor.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+
+[CustomEditor(typeof(Highlighter))]
+public class HighlighterEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
--- Assets/HighlightOnMouseOver.cs
+++ Assets/HighlightOnMouseOver.cs
@@ -6,52 +6,27 @@ using System.Linq;
 
 public class HighlightOnMouseOver : MonoBehaviour
 {
-    [SerializeField] GameObject highlightPrefab;
     IClickChecker tileClickerListener;
-    ObjectPool<GameObject> pool;
-
+    [SerializeField] Highlighter highlighter;
     [SerializeField] int highlightRange;
-    [SerializeField] Vector2 position;
-    Dictionary<Location, GameObject> highlightedTiles = new();
 
     // Start is called before the first frame update
-    private void Start()
+    private void OnEnable()
     {
-        
-        pool = new ObjectPool<GameObject>(() => { return Instantiate(highlightPrefab); }, highlight => { highlight.SetActive(true); highlight.transform.position = position; },
-            highlight => { highlight.SetActive(false); }, highlight => { Destroy(highlight); }, true, 30, 150);
-
         tileClickerListener = GlobalClickListener.Instance.MapTileListener;
-        tileClickerListener.EntityMousedOver += HighlightTile;
+        tileClickerListener.EntityMousedOver += HighlightTiles;
     }
 
-    void HighlightTile(ClickableEntity entity)
+    private void OnDisable()
+    {
+        tileClickerListener = GlobalClickListener.Instance.MapTileListener;
+        tileClickerListener.EntityMousedOver -= HighlightTiles;
+        highlighter.StopHighlighting();
+    }
+
+    public void HighlightTiles(ClickableEntity entity)
     {
         var locationsToHighlight = LocationUtility.LocationsInSquareRadius(entity.GetComponent<IGameEntity>().Location, highlightRange).ToHashSet();
-        List<Location> locationsToRemove = new();
-
-        foreach (var location in highlightedTiles)
-        {
-            if (!locationsToHighlight.Contains(location.Key))
-            {
-                locationsToRemove.Add(location.Key);
-            }
-        }
-
-        foreach (var location in locationsToHighlight)
-        {
-            if (!highlightedTiles.ContainsKey(location) && LocationUtility.IsOnMap(location))
-            {
-                position = new Vector2(location.X, location.Y);
-                highlightedTiles.Add(location, pool.Get());
-            }
-        }
-
-        foreach (var location in locationsToRemove)
-        {
-            pool.Release(highlightedTiles[location]);
-            highlightedTiles.Remove(location);
-        }
-        locationsToRemove.Clear();
+        highlighter.HighlightTiles(locationsToHighlight);
     }
 }
