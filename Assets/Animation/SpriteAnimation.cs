62b464b7976eb6c2b61e1889638a14258eb32e35
Daniel <61716202+WarbDev@users.noreply.github.com>
2022-11-11 12:52:54 -0800

    Update SpriteAnimation.cs

--- Assets/Animation/SpriteAnimation.cs
+++ Assets/Animation/SpriteAnimation.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SpriteAnimation : EntityAnimation<SpriteAnimationProperties>
 {
+    [SerializeField] SpriteSet spriteSet;
+    Coroutine spriteUpdateCoroutine;
+
+    void Start()
+    {
+        StartCoroutine(UpdateSprite());    
+    }
+
     public override void Pause()
     {
         enabled = false;
@@ -11,7 +20,15 @@ public class SpriteAnimation : EntityAnimation<SpriteAnimationProperties>
 
     public override void Play(SpriteAnimationProperties animationProperties)
     {
-        throw new System.NotImplementedException();
+        spriteUpdateCoroutine = StartCoroutine(UpdateSprite());
+    }
+
+    IEnumerator UpdateSprite()
+    {
+        while (true)
+        {
+
+        }
     }
 
     public override void Unpause()
@@ -22,5 +39,17 @@ public class SpriteAnimation : EntityAnimation<SpriteAnimationProperties>
 
 public class SpriteAnimationProperties : IAnimationProperties
 {
+    SpriteRenderer targetRenderer;
+    Func<float> timeElapsedCalc;
+    float duration;
 
+    public SpriteRenderer TargetRenderer { get => targetRenderer; }
+    public Func<float> TimeElapsedCalc { get => timeElapsedCalc; }
+    public float Duration { get => duration; }
+    public SpriteAnimationProperties(SpriteRenderer targetRenderer, Func<float> timeElapsedCalc, float duration)
+    {
+        this.targetRenderer = targetRenderer;
+        this.timeElapsedCalc = timeElapsedCalc;
+        this.duration = duration;
+    }
 }
