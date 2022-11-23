94d8b9654f96ce94d74feab0576e94fe2e9ba4d2
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-11-23 14:03:28 -0800

    Update DamageableComponent.cs

--- Assets/GameEntities/DamageableComponent.cs
+++ Assets/GameEntities/DamageableComponent.cs
@@ -14,8 +14,11 @@ public abstract class DamageableComponent : EntityComponent, IDamageable
 
 public interface IDamageable : IHaveGameEntity
 {
-    public IDamageable Damageable { get; }
+    public IDamageable Damageable { get; } //Suggested: Use DamageableComponent, attached to the Prefab that IDamageable is attached to
 
+
+    //Don't implement DealDamage in the main class that is attached to a prefab.
+    //Implement it through the Damageable Property.
     public DamageLog DealDamage(DamageDetails damage)
     {
         return Damageable.DealDamage(damage);
