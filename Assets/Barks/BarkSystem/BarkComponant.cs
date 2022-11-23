de78db3e7517da0b9ef64bd32a607ab3805054a6
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-11-22 17:35:44 -0800

    test

---
+++ Assets/Barks/BarkSystem/BarkComponant.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarkComponant : MonoBehaviour
+{
+
+    [SerializeField] List<AudioClip> SpeakClips;
+    [SerializeField] List<AudioClip> HurtClips;
+    [SerializeField] AudioClip DieClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
