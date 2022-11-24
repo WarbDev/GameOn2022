791ea90908be88c5fd7b021784c6f54f6931bc3b
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-11-23 19:16:12 -0800

    Added Textboxes

--- Assets/Barks/BarkSystem/Bark.cs
+++ Assets/Barks/BarkSystem/Bark.cs
@@ -10,19 +10,32 @@ public class Bark : MonoBehaviour
 
     [SerializeField] List<AudioClip> Clips;
     [SerializeField] List<string> Words;
-    [SerializeField] GameObject TextBox;
+    [SerializeField] GameObject TextBox; //This TextBox is the child of gameObject
 
     public void PlayBark(GameObject player)
     {
         int randNumber = Random.Range(1, Clips.Count);
         GlobalAudioSource.Instance.Play(Clips[randNumber]);
-        randNumber = Random.Range(1, Words.Count);
+
+        SetText(player);
+    }
+
+    private void SetText(GameObject player)
+    {
+
+        int randNumber = Random.Range(1, Words.Count);
         gameObject.GetComponent<TextMeshPro>().SetText(Words[randNumber]);
+
         GameObject textObject = Instantiate(gameObject);
+        textObject.transform.SetParent(player.transform, false);
 
-        textObject.transform.SetParent(WorldCanvasSingleton.Instance.transform, false);
+        SpriteRenderer textBoxRenderer = TextBox.GetComponent<SpriteRenderer>();
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
 
-        textObject.transform.position = player.transform.position;
+        rectTransform.sizeDelta = 
+            new Vector2(textBoxRenderer.bounds.size.x, textBoxRenderer.bounds.size.y);
+
+
 
     }
 }
