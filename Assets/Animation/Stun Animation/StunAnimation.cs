5a383534fa9737c72ff72d5442b09732e099fe5d
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-11-29 18:35:02 -0800

    added stun animation

---
+++ Assets/Animation/Stun Animation/StunAnimation.cs
@@ -0,0 +1,40 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunAnimation : EntityAnimation<JumpAnimationProperties>
+{
+    Sequence currentlyPlaying;
+
+    public override Sequence CurrentlyPlaying { get => currentlyPlaying; }
+
+    public override event Action<EntityAnimation<JumpAnimationProperties>> AnimationFinished;
+    [SerializeField] Transform affectedTransform;
+    [SerializeField] float duration;
+    [SerializeField] float intensity;
+    [SerializeField] int vibrato;
+    [SerializeField] AudioClip clip = null;
+
+
+    public override void Play(JumpAnimationProperties animationProperties)
+    {
+        Sequence chargeSequence = DOTween.Sequence();
+        float strength = intensity;
+        chargeSequence.Append(affectedTransform.DOShakePosition(duration, new Vector3(strength, strength), vibrato, 90, false, false, ShakeRandomnessMode.Harmonic)).OnComplete(onComplete);
+
+        if (clip == null)
+        {
+            GlobalAudioSource.Instance.Play(clip);
+        }
+
+        currentlyPlaying = chargeSequence;
+        void onComplete() => AnimationFinished?.Invoke(this);
+    }
+}
+
+public class StunAnimationProperties : IAnimationProperties
+{
+
+}
