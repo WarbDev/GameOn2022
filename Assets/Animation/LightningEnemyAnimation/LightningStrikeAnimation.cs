9d6d5425cb0bfe6a62051f58507c6bae4ac374f9
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-11-30 19:44:17 -0800

    Changed CancelScript

--- Assets/Animation/LightningEnemyAnimation/LightningStrikeAnimation.cs
+++ Assets/Animation/LightningEnemyAnimation/LightningStrikeAnimation.cs
@@ -19,27 +19,28 @@ public class LightningStrikeAnimation : EntityAnimation<LightningStrikePropertie
 
     public override event Action<EntityAnimation<LightningStrikeProperties>> AnimationFinished;
 
+    private LightningStrikeProperties props;
+
     public override void Play(LightningStrikeProperties properties)
     {
+        props = properties;
         currentlyPlaying = DOTween.Sequence();
 
         ChargeAnimationProperties chargeProp = new(2);
-        chargeAnimation.Play(chargeProp);
-        chargeAnimation.AnimationFinished += onComplete;
+        //chargeAnimation.Play(chargeProp);
+        //chargeAnimation.AnimationFinished += onComplete;
 
 
         // Join together the primary animation with the sprite animation, and announce completion when done.
-        void onComplete(EntityAnimation<ChargeAnimationProperties> a)
-        {
-            foreach (var damageLog in properties.damageLogs)
-            {
-                IAnimatable animatable = damageLog.Target.Entity as IAnimatable;
-                animatable.PlayAnimation<HurtAnimationProperties>(ANIMATION_ID.ENTITY_HURT, new(damageLog));
-            }
-            a.AnimationFinished -= onComplete;
+        //void onComplete(EntityAnimation<ChargeAnimationProperties> a)
+        //{
+            
+           // a.AnimationFinished -= onComplete;
 
+            gameObject.SetActive(true);
             enemySpriteAnimation.Play(new(enemySpriteRenderer));
 
+            
             StartCoroutine(LightningBolt());
 
             enemySpriteAnimation.AnimationFinished += finished;
@@ -47,21 +48,28 @@ public class LightningStrikeAnimation : EntityAnimation<LightningStrikePropertie
             void finished(EntityAnimation e)
             {
                 AnimationFinished?.Invoke(this);
+                gameObject.SetActive(false);
             }
-        } 
+        //} 
     }
 
     private IEnumerator LightningBolt()
     {
         yield return new WaitForSeconds(.5f);
-        gameObject.SetActive(true);
+
+        foreach (var damageLog in props.damageLogs)
+        {
+            IAnimatable animatable = damageLog.Target.Entity as IAnimatable;
+            animatable.PlayAnimation<HurtAnimationProperties>(ANIMATION_ID.ENTITY_HURT, new(damageLog));
+        }
+
         lightningSpriteAnimation.Play(new(gameObject.GetComponent<SpriteRenderer>()));
         lightningSpriteAnimation.AnimationFinished += finished;
 
 
         void finished(EntityAnimation e)
         {
-            gameObject.SetActive(false);
+            
         }
     }
 }
