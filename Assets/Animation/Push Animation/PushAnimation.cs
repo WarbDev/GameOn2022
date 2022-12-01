b21a0c9fa68147600966dd284d1ed9da26c90b81
Henry <63266437+ZidixMalock@users.noreply.github.com>
2022-12-01 00:23:05 -0800

    Implimented push animation

---
+++ Assets/Animation/Push Animation/PushAnimation.cs
@@ -0,0 +1,36 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushAnimation : EntityAnimation<PushAnimationProperties>
+{
+    public override Sequence CurrentlyPlaying { get => currentlyPlaying; }
+    public override event Action<EntityAnimation<PushAnimationProperties>> AnimationFinished;
+
+    private Sequence currentlyPlaying;
+
+    public override void Play(PushAnimationProperties animationProperties)
+    {
+        currentlyPlaying = DOTween.Sequence();
+
+        Location distance = animationProperties.PushLog.MoveLog.End - animationProperties.PushLog.MoveLog.Start;
+        float time = MathF.Sqrt(Mathf.Pow(distance.X, 2) + Mathf.Pow(distance.Y, 2));
+
+
+        currentlyPlaying.Append(animationProperties.PushLog.MoveLog.Entity.transform.DOMove(LocationUtility.LocationToVector3(animationProperties.PushLog.MoveLog.End), time).SetEase(Ease.Linear));
+    }
+}
+
+public class PushAnimationProperties : IAnimationProperties
+{
+    PushLog pushLog;
+
+    public PushAnimationProperties(PushLog pushLog)
+    {
+        this.pushLog = pushLog;
+    }
+
+    public PushLog PushLog { get => pushLog;}
+}
